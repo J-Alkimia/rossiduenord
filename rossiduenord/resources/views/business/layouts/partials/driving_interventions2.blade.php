b6d57009ad02dad6e45314794bdf531fd1954eef
valerio-m-alkimia <valerio.m@alkimiadigital.it>
2022-03-11 18:16:39 +0100

    wip page3 superbonus

--- rossiduenord/resources/views/business/layouts/partials/driving_interventions2.blade.php
+++ rossiduenord/resources/views/business/layouts/partials/driving_interventions2.blade.php
@@ -97,6 +97,119 @@
                 </tr>
             </tbody>
         </table>
+        <div class="d-flex mt-5" style="background-color: #f2f2f2; width:100%; padding:5px 10px">
+            <div class="d-flex mr-4">
+                <p class="m-0">Totale “pareti verticali”</p>
+                <label for="" class="m-0 black">
+                    <input type="text" value="656,46" class="border ml-2 px-2 text-right" style="width: 80px">
+                    m²
+                </label>
+            </div>
+            <div class="d-flex mr-4">
+                <p class="m-0">di cui realizzati SAL n. 1</p>
+                <label for="" class="m-0  black">
+                    <input type="text" value="557,99" class="border ml-2 px-2 text-right" style="width: 80px">
+                    m²
+                </label>
+            </div>
+            <div class="d-flex mr-4">
+                <p class="m-0">SAL n. 2</p>
+                <label for="" class="m-0  black">
+                    <input type="text" value="65,65" class="border ml-2 px-2 text-right" style="width: 80px">
+                    m²
+                </label>
+            </div>
+            <div class="d-flex mr-4">
+                <p class="m-0">SAL F.</p>
+                <label for="" class="m-0  black">
+                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 80px">
+                    m²
+                </label>
+            </div>
+        </div>
+        <div class="d-flex align-items-center mt-3" style="width:100%;">
+            <p class="m-0">Superficie totale oggetto dell’intervento</p>
+            <label for="" class=" m-0 mr-4 black">
+                <input type="text" value="1.045,81" style="width: 80px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                m²
+            </label>
+        </div>
         
+        <div class="d-flex align-items-center justify-content-between mt-3" style="width:100%;">
+            <div class="d-flex align-items-center">
+                <p class="m-0">Il costo complessivo previsto in progetto dei lavori sulle pratiche opache ammonta a*</p>
+                <label for="" class=" m-0 mr-4 black">
+                    <input type="text" value="500.115,13" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                    €
+                </label>
+            </div>
+            <button class="add-button">Computo metrico</button>
+        </div>
+        
+        <p class="m-0 mt-2 font-italic">* Incluso iva e spese professionali (es. progettazione, direzione lavori, assservazione tecnica e fiscale)</p>
+        
+        <label class="checkbox-wrapper d-flex mt-5">
+            <input type="checkbox" name="" value="">     
+            <span class="checkmark"></span> 
+            <span class="black" ><b>IN. Sostituzione degli infissi</b></span>
+        </label>
+
+        <p>Le superfici oggetto dell'intervento sono:</p>
+
+        <table class="table_bonus" style="width: 100%">
+            <thead>
+                <tr>
+                    <td class="text-center" style="width:5%;"><b>N.</b></td>
+                    <td style="width:15%;"><b>Descrizione</b></td>
+                    <td style="width:10%;"><b>Superficie (m2)</b></td>
+                    <td style="width:10%;">
+                        <b>
+                            Trasm. ante
+                            (W/m2k)    
+                        </b>
+                    </td>
+                    <td style="width:10%;">
+                        <b>
+                            Trasm. post
+                            (W/m2k)    
+                        </b>
+                    </td>
+                    <td style="width:10%;">
+                        <b>Telaio prima</b>
+                    </td>
+                    <td style="width:10%;"><b>Vetro prima</b></td>
+                    <td style="width:10%;"><b>Telaio dopo</b></td>
+                    <td style="width:10%;"><b>Vetro dopo</b></td>
+                    <td style="width:10%;"><b>Oscurante</b></td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="text-center">1</td>
+                    <td class="text-center"></td>
+                    <td class="text-right">0,00</td>
+                    <td class="text-right">0,00</td>
+                    <td class="text-right">0,00</td>
+                    <td class="text-right"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div class="d-flex align-items-center justify-content-between mt-5" style="width:100%;">
+            <div class="d-flex align-items-center">
+                <p class="m-0">Le spese previste in progetto dei lavori al punto IN ammontano a*</p>
+                <label for="" class=" m-0 mr-4 black">
+                    <input type="text" value="500.115,13" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                    €
+                </label>
+            </div>
+            <button class="add-button">Computo metrico</button>
+        </div>
+        <p class="m-0 mt-2 font-italic">* Incluso iva e spese professionali (es. progettazione, direzione lavori, assservazione tecnica e fiscale)</p>
+
     </div>
 </div>
