7c9cdad6a520b4317c0f36407611351acb133a95
daniele-pipino-alkimiadigital <daniele.p@alkimiadigital.it>
2022-03-16 12:51:59 +0100

    fees page layout complete

--- rossiduenord/resources/views/business/layouts/partials/fees_declarations.blade.php
+++ rossiduenord/resources/views/business/layouts/partials/fees_declarations.blade.php
@@ -6,175 +6,254 @@
 </div>
 {{-- ecobonus section --}}
 <section>
-    <div>
-        <h4 class="px-20">Ecobonus</h4>
-        {{-- box --}}
-       <div class="box">
-            {{-- first --}}
-            <div class="row px-20">
-                <div class="col-5">
-                    <div>
-                        <p class="m-0">Costo complessivo degli intereventi trainanti di porgetto ammontano a  <br>    (compreso importo trainati parti comuni)</p> 
+    {{-- box --}}
+    <div class="box px-20">
+            <h4 class="blue">Ecobonus:</h4>
+            {{-- first row --}}
+            <div class="row">
+                <div class="col-6">
+                    <p>Costo complessivo degli interventi trainanti di progetto ammontano a <br>  (compreso impoprto trainati parti comuni) </p>
                 </div>
-                </div>
-                <div class="col-1">
+                <div class="col-3">
                     <label for="" class=" m-0 mr-4 black">
-                        <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        €
                     </label>
                 </div>
             </div>
-            
-            
-            {{-- second --}}
-            <div class="row mt-3 px-20">
-                    <div class="col-5">
-                        <div>
-                            <p class="m-0">Costo complessivo degli intereventi trainanti realizzati ammonta a  <br>    (compreso importo trainati parti comuni)</p> 
+            {{-- second row --}}
+            <div class="row">
+                <div class="col-5">
+                    <p>Costo complessivo degli interventi trainanti realizzati ammontano a <br>   (compreso importo trainati parti comini) </p>
+                </div>
+                <div class="col-7 d-flex flex-wrap">
+                    <div class="m-1">
+                        <label for="" class=" m-0 black">
+                            <span>Sal n.1</span>
+                            <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                            <span>€</span>
+                        </label>
                     </div>
+                    <div class="m-1">
+                        <label for="" class=" m-0 black">
+                            <span>Sal n.2</span>
+                            <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                            <span>€</span>
+                        </label>
                     </div>
-                    <div class="col-7 d-flex flex-wrap">
-                        {{-- salaries --}}
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.1</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.2</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.F</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.1 + 2</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.1 + 2n.F</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                    
-                        {{-- salaries --}}
+                    <div class="m-1">
+                        <label for="" class=" m-0 black">
+                            <span>Sal n.F</span>
+                            <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                            <span>€</span>
+                        </label>
                     </div>
                 </div>
-            
+                <div class="col-7 offset-5 d-flex justify-content-end">
+                    <div class="m-1">
+                        <label for="" class=" m-0 black">
+                            <span>Sal n.1+2</span>
+                            <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                            <span>€</span>
+                        </label>
+                    </div>
+                    <div class="m-1">
+                        <label for="" class=" m-0 black">
+                            <span>Sal n.1+2n.F</span>
+                            <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                            <span>€</span>
+                        </label>
+                    </div>
+                </div>
             </div>
-       </div>
+        </div>
+        
        {{-- divider --}}
-       <hr class="divider">
-       {{-- box --}}
-       <div class="box">
-          {{-- first --}}
-            <div class="row px-20">
-                <div class="col-5">
-                    <div>
-                        <p class="m-0">Costo complessivo degli intereventi trainanti di porgetto ammontano a  <br>    (compreso importo trainati parti comuni)</p> 
+    </div>
+    <hr class="divider">
+    {{-- box --}}
+    <div class="box px-20">
+        {{-- first row --}}
+        <div class="row">
+            <div class="col-6">
+                <p>Costo complessivo degli interventi trainanti di progetto ammontano a</p>
+            </div>
+            <div class="col-3">
+                <label for="" class=" m-0 mr-4 black">
+                    <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                    €
+                </label>
+            </div>
+        </div>
+        {{-- second row --}}
+        <div class="row">
+            <div class="col-5">
+                <p>Costo complessivo degli interventi trainanti realizzati ammontano a</p>
+            </div>
+            <div class="col-7 d-flex flex-wrap">
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.1</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
                 </div>
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.2</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
                 </div>
-                <div class="col-1">
-                    <label for="" class=" m-0 mr-4 black">
-                        <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.F</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
                     </label>
                 </div>
             </div>
-            {{-- second --}}
-            <div class="row mt-3 px-20">
-                    <div class="col-5">
-                        <div>
-                            <p class="m-0">Costo complessivo degli intereventi trainanti realizzati ammonta a  <br>    (compreso importo trainati parti comuni)</p> 
-                    </div>
-                    </div>
-                    <div class="col-7 d-flex flex-wrap">
-                        {{-- salaries --}}
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.1</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.2</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.F</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.1 + 2</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                        <div class="col-4 d-flex">
-                            <div>
-                                <p>Sal n.1 + 2n.F</p>
-                            </div>
-                            <div>
-                                <label for="" class=" m-0 black">
-                                    <input type="text" value="" class="border ml-2 px-2 text-right" style="width: 120px; background-color: rgb(242, 242, 242);">€
-                                </label>
-                            </div>  
-                        </div>
-                    
-                        {{-- salaries --}}
-                    </div>
+            <div class="col-7 offset-5 d-flex justify-content-end">
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.1+2</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
                 </div>
-            
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.1+2n.F</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
             </div>
-      </div>
-       
+        </div>
     </div>
-    
+   {{-- divider --}}
+   <hr class="divider">
+   {{-- box --}}
+   <div class="box px-20">
+        {{-- first row --}}
+        <div class="row">
+            <div class="col-6">
+                <p>L'importo degli interventi trainati di progetto ammonata a </p>
+            </div>
+            <div class="col-3">
+                <label for="" class=" m-0 mr-4 black">
+                    <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                    €
+                </label>
+            </div>
+        </div>
+        {{-- second row --}}
+        <div class="row">
+            <div class="col-5">
+                <p>L'importo degli interventi realizzati corrisponde a </p>
+            </div>
+            <div class="col-7 d-flex flex-wrap">
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.1</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.2</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.F</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+            </div>
+            <div class="col-7 offset-5 d-flex justify-content-end">
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.1+2</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.1+2n.F</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+            </div>
+        </div>
+   </div>
+   {{-- divider --}}
+   <hr class="divider">
+   {{-- box --}}
+   <div class="box px-20">
+        <h4 class="blue">Sismabonus</h4>
+        {{-- first row --}}
+        <div class="row">
+            <div class="col-6">
+                <p>L'importo degli interventi di progetto ammonta a</p>
+            </div>
+            <div class="col-3">
+                <label for="" class=" m-0 mr-4 black">
+                    <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                    €
+                </label>
+            </div>
+        </div>
+        {{-- second row --}}
+        <div class="row">
+            <div class="col-5">
+                <p>L'importo degli interventi realizzati ammonta a</p>
+            </div>
+            <div class="col-7 d-flex flex-wrap">
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.1</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.2</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.F</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+            </div>
+            <div class="col-7 offset-5 d-flex justify-content-end">
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.1+2</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+                <div class="m-1">
+                    <label for="" class=" m-0 black">
+                        <span>Sal n.1+2n.F</span>
+                        <input type="text" value="" style="width: 120px; background-color: #f2f2f2" class="border ml-2 px-2 text-right">
+                        <span>€</span>
+                    </label>
+                </div>
+            </div>
+        </div>
+   </div>
 </section>
